96f0e90e8cfbce168a23b4b49c62b499e527a453
surajkrishna <suraj.k.25@gmail.com>
2025-12-19 18:48:39 +0530

    my initial commit

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Home Page Suraj !
             </Typography>
             <Typography
               variant="h5"
